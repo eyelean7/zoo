89b11612811b9d55eaad2b227cacaaf1c1b366de
Dan Jacob and Ilene Gorski <dan+ilene@>
2017-06-08 09:32:11 -0700

    add checkbox to toggle if Keg is empty

--- app/emptiness.pipe.ts
+++ app/emptiness.pipe.ts
@@ -7,13 +7,24 @@ import {Keg} from './keg.model';
 })
 
 export class EmptinessPipe implements PipeTransform {
-  transform(input: Keg[]) {
+  transform(input: Keg[], desiredEmptiness) {
     var output: Keg[] = [];
+    if(desiredEmptiness === "nonEmptyKegs") {
     for (var i=0; i < input.length; i++) {
-      if (input[i].empty === true) {
+      if (input[i].empty === false) {
         output.push(input[i]);
       }
     }
     return output;
+  } else if (desiredEmptiness === "emptyKegs") {
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].empty === true) {
+        output.push(input[i]);
+      }
+    }
+  return output;
+} else {
+  return input
+    }
   }
 }
--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -4,21 +4,32 @@ import { Keg } from './keg.model';
 @Component ({
   selector: 'keg-list',
   template: `
-    <div *ngFor="let currentKeg of childKegList | emptiness">
-      <h3 (click)="showInfo">{{currentKeg.name}} <button (click)="editButtonHasBeenClicked(currentKeg)">Edit!</button></h3>
+  <select (change)="onChange($event.target.value)">
+    <option value="allKegs" selected="selected">All Kegs</option>
+    <option value="emptyKegs">Empty Kegs</option>
+    <option value="nonEmptyKegs"> Not Empty Kegs</option>
+  </select>
+
+
+  <ul>
+    <li (click)="isDone(currentKeg)" *ngFor="let currentKeg of childKegList | emptiness:filterbyEmptiness"><h3>{{currentKeg.name}}</h3>
       <ul>
         <li>{{currentKeg.brand}}</li>
         <li>{{currentKeg.price}}</li>
-        <li>{{currentKeg.alcoholContent}} proof</li>
+        <li>{{currentKeg.alcoholContent}}</li>
+        <input *ngIf="currentKeg.empty === true" type="checkbox" checked (click)="toggleDone(currentKeg, false)"/>
+        <input *ngIf="currentKeg.empty === false" type="checkbox" (click)="toggleDone(currentKeg, true)"/>
+        <button (click)="editButtonHasBeenClicked(currentKeg)">Edit!</button>
       </ul>
-    </div>
+    </li>
+  </ul>
   `
 })
 
  export class KegListComponent {
    @Input() childKegList: Keg[];
    @Output() clickSender = new EventEmitter();
-
+   filterbyEmptiness: string = "allKegs"
    editButtonHasBeenClicked(kegToEdit: Keg) {
      this.clickSender.emit(kegToEdit)
    }
@@ -29,6 +40,14 @@ import { Keg } from './keg.model';
      } else {
        alert("Not empty!");
      }
+   }
+
+   onChange(optionFromMenu){
+     this.filterbyEmptiness = optionFromMenu;
+   }
+
+   toggleDone(clickedKeg: Keg, setEmptiness: boolean){
+     clickedKeg.empty = setEmptiness;
    }
 
  }
