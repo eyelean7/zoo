0c999df2656846f23c8fea7b2508165f093d28fb
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-09 09:22:33 -0700

    split new animal form into 2 columns

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -5,44 +5,49 @@ import { Animal } from './animal.model';
   selector: 'new-animal',
   template: `
   <h1 class="banner">New Animal</h1>
-  <form>
-  <div>
-    <label>Enter Animal Species:</label>
-    <input #newSpecies style="font-size:25px" type="text" class="form-control"/>
-  </div>
-    <div>
-      <label>Enter Animal Name:</label>
-      <input #newName style="font-size:25px" type="text" class="form-control"/>
+  <form class="row">
+    <div class="col-md-6">
+      <div>
+        <label>Enter Animal Species:</label>
+        <input #newSpecies style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Name:</label>
+        <input #newName style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Age:</label>
+        <input #newAge style="font-size:25px" type="number" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Diet:</label>
+        <input #newDiet style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Location:</label>
+        <input #newLocation style="font-size:25px" type="text" class="form-control"/>
+      </div>
     </div>
-    <div>
-      <label>Enter Animal Age:</label>
-      <input #newAge style="font-size:25px" type="number" class="form-control"/>
+    <div class="col-md-6">
+      <div>
+        <label>Enter Number of Caretakers:</label>
+        <input #newCaretakers style="font-size:25px" type="number" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Sex:</label>
+        <input #newSex style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Likes:</label>
+        <input #newLikes style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <div>
+        <label>Enter Animal Dislikes:</label>
+        <input #newDislikes style="font-size:25px" type="text" class="form-control"/>
+      </div>
+      <br/>
+      <button class="btn btn-success" (click)="submitForm(newName.value, newBrand.value, newPrice.value, newAlcoholContent.value);newName.value='';newBrand.value='';newPrice.value='';newAlcoholContent.value='';">Add</button>
     </div>
-    <div>
-      <label>Enter Animal Diet:</label>
-      <input #newDiet style="font-size:25px" type="text" class="form-control"/>
-    </div>
-    <div>
-      <label>Enter Animal Location:</label>
-      <input #newLocation style="font-size:25px" type="text" class="form-control"/>
-    </div>
-    <div>
-      <label>Enter Number of Caretakers:</label>
-      <input #newCaretakers style="font-size:25px" type="number" class="form-control"/>
-    </div>
-    <div>
-      <label>Enter Sex:</label>
-      <input #newSex style="font-size:25px" type="text" class="form-control"/>
-    </div>
-    <div>
-      <label>Enter Animal Likes:</label>
-      <input #newLikes style="font-size:25px" type="text" class="form-control"/>
-    </div>
-    <div>
-      <label>Enter Animal Dislikes:</label>
-      <input #newDislikes style="font-size:25px" type="text" class="form-control"/>
-    </div>
-    <button class="btn btn-success" (click)="submitForm(newName.value, newBrand.value, newPrice.value, newAlcoholContent.value);newName.value='';newBrand.value='';newPrice.value='';newAlcoholContent.value='';">Add</button>
   </form>
   `
 })
